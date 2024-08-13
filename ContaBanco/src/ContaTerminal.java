49536aa5233f985ef18cf5700913b106bb361b76
Caio Emerick <caiopros@hotmail.com>
2024-08-13 14:05:16 -0300

    Desafio-Conta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,10 @@
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    
+    String agencia, nome;
+    int numero;
+    double saldo;
+
+    
+    
 }
---
+++ ContaBanco/src/Usuario.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Usuario {
+    
+     Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.println("Por favor, digite o número da Agência !");
+        conta.agencia = scanner.next();
+        System.out.println("digite o número da Conta:")
+        conta.numero = scanner.nextInt();
+        System.out.println("digite o Nome:");
+        conta.nome = scanner.next();
+        System.out.println("digite o saldo:")
+        conta.saldo = scanner.next();
+}
